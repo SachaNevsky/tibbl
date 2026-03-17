e38ea7aad9bd2686bbc6d63af167c86b27fd0f41
SachaNevsky <sacha.nevsky@gmail.com>
2026-03-17 17:27:41 +0100

    Updates

---
+++ assets/root-B9lsoZzB.js
@@ -0,0 +1,8 @@
+import{w as a,q as i,p as t,M as c,L as l,S as p,t as h,O as u,i as d}from"./chunk-EPOLDU6W-Cbkwubdd.js";const f=()=>[{rel:"preconnect",href:"https://fonts.googleapis.com"},{rel:"preconnect",href:"https://fonts.gstatic.com",crossOrigin:"anonymous"},{rel:"stylesheet",href:"https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"},{rel:"stylesheet",href:"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"},{rel:"icon",href:"/tibbl/favicon.ico",type:"image/ico"}];function x({children:e}){return t.jsxs("html",{lang:"en",children:[t.jsxs("head",{children:[t.jsx("meta",{charSet:"utf-8"}),t.jsx("meta",{name:"viewport",content:"width=device-width, initial-scale=1"}),t.jsx("script",{dangerouslySetInnerHTML:{__html:`
+            (function() {
+              var p = new URLSearchParams(window.location.search).get('p');
+              if (p) {
+                window.history.replaceState(null, '', '/tibbl' + p);
+              }
+            })();
+          `}}),t.jsx("title",{children:"TIBBL"}),t.jsx(c,{}),t.jsx(l,{})]}),t.jsxs("body",{children:[e,t.jsx(p,{}),t.jsx(h,{})]})]})}const j=a(function(){return t.jsx(u,{})}),w=i(function({error:s}){let n="Oops!",o="An unexpected error occurred.",r;return d(s)&&(n=s.status===404?"404":"Error",o=s.status===404?"The requested page could not be found.":s.statusText||o),t.jsxs("main",{className:"pt-16 p-4 container mx-auto",children:[t.jsx("h1",{children:n}),t.jsx("p",{children:o}),r]})});export{w as ErrorBoundary,x as Layout,j as default,f as links};
